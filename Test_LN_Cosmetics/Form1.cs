389cc0e59daad9944833938c9fe8453212485329
EskiKedi <ponomarevase@mail.ru>
2016-12-21 20:04:44 +0700

    Добавление

---
+++ Test_LN_Cosmetics/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+
+namespace Test_LN_Cosmetics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<Price> ReadFromExcel(OleDbConnection connection, out String err)
+        {
+            err = "";
+            List<Price> res = new List<Price>();
+            OleDbCommand command;
+            OleDbDataReader reader=null;
+            try
+            {
+                command = new OleDbCommand("SELECT * FROM [Лист2$]", connection);
+                reader = command.ExecuteReader();
+                reader.Read();
+                int j = 0;
+                do
+                {
+                    j += 1;
+                    if (j < 3) continue;
+                    Price r = new Price();
+                    r.Cod = int.Parse(reader[0].ToString());
+                    r.Name = reader[1].ToString();
+                    r.MNF = reader[2].ToString();
+                    r.CNTR = reader[3].ToString();
+                    r.Srok = DateTime.Parse(reader[4].ToString());
+                    r.Kol = int.Parse(reader[5].ToString());
+                    r.Cena = decimal.Parse(reader[6].ToString());
+                    r.Kratnost = int.Parse(reader[7].ToString());
+                    r.Barcode = Int64.Parse(reader[8].ToString());
+                    r.Ratends = int.Parse(reader[9].ToString());
+                    res.Add(r);
+
+
+                } while (reader.Read());
+            }
+            catch(Exception e)
+            {
+                err = e.Message;
+            }
+            finally
+            {
+                if(reader.IsClosed==false) reader.Close();
+                reader.Dispose();
+               
+            }
+            return res;
+        }
+
+        public void CreateTextFile(string fName, List<Price> PriceList, out String err)
+        {
+            err = "";
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(fName, true, Encoding.UTF8))
+                {
+                    StringBuilder str = new StringBuilder();
+                    str.Append("{\"providerName\": \"ООО НПЛ \"ЛН-Косметика\",");
+                    file.WriteLine(str.ToString());
+                    str = new StringBuilder();
+                    str.Append("\"updateDate\": \"");
+                    str.Append(DateTime.Today.ToShortDateString());
+                    str.Append("\",");
+                    file.WriteLine(str.ToString());
+
+
+                    str = new StringBuilder();
+                    str.Append("\"items\":[{");
+                    file.WriteLine(str.ToString());
+                    int i = 0;
+                    foreach (Price item in PriceList)
+                    {
+                        if (i > 0)
+                        {
+                            str = new StringBuilder();
+                            str.Append("},{");
+                            file.WriteLine(str.ToString());
+                        }
+                        //Код препарата
+                        str = new StringBuilder();
+                        str.Append("\"code\": \"");
+                        str.Append(item.Cod.ToString());
+                        str.Append("\",");
+                        file.WriteLine(str.ToString());
+
+                        //Название препарата
+                        str = new StringBuilder();
+                        str.Append("\"name\": \"");
+                        str.Append(item.Name.ToString());
+                        str.Append("\",");
+                        file.WriteLine(str.ToString());
+
+                        //Производитель
+                        str = new StringBuilder();
+                        str.Append("\"manufacturer\": \"");
+                        str.Append(item.MNF.ToString());
+                        str.Append("\",");
+                        file.WriteLine(str.ToString());
+
+
+                        //Страна
+                        str = new StringBuilder();
+                        str.Append("\"manufacturerCountry\": \"");
+                        str.Append(item.CNTR.ToString());
+                        str.Append("\",");
+                        file.WriteLine(str.ToString());
+
+                        //СКол-во товара на складе поставщика
+                        str = new StringBuilder();
+                        str.Append("\"quantity\": ");
+                        str.Append(item.Kol.ToString());
+                        str.Append(",");
+                        file.WriteLine(str.ToString());
+
+                        //Штрих-код
+                        str = new StringBuilder();
+                        str.Append("\"barcode\": \"");
+                        str.Append(item.Barcode.ToString());
+                        str.Append("\",");
+                        file.WriteLine(str.ToString());
+
+                        //Цена
+                        str = new StringBuilder();
+                        str.Append("\"price\": ");
+                        str.Append(item.Cena.ToString());
+                        str.Append(",");
+                        file.WriteLine(str.ToString());
+
+                        //Крастность
+                        str = new StringBuilder();
+                        str.Append("\"multiplisity\": ");
+                        str.Append(item.Kratnost.ToString());
+                        str.Append(",");
+                        file.WriteLine(str.ToString());
+
+                        //НДС
+                        str = new StringBuilder();
+                        str.Append("\"ratends\": ");
+                        str.Append(item.Ratends.ToString());
+                        str.Append(",");
+                        file.WriteLine(str.ToString());
+
+                        //Срок годности
+                        str = new StringBuilder();
+                        str.Append("\"exprirationDate\": \"");
+                        str.Append(item.Srok.ToShortDateString());
+                        str.Append("\",");
+                        file.WriteLine(str.ToString());
+                        i += 1;
+
+                    }
+
+                    str = new StringBuilder();
+                    str.Append("}]}");
+                    file.WriteLine(str.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                err = e.Message;
+                return;
+            }
+            }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+        }
+
+         public static void SendToFTP (string ArchName, string FTPServerName)
+        {
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FTPServerName);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+          
+            request.Credentials = new NetworkCredential ("anonymous","ponomarevase@mail.ru");
+
+           
+            StreamReader sourceStream = new StreamReader(ArchName);
+            byte [] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+
+            response.Close();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+           // string SourseFilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.xls");
+            string SourseFilePath = System.IO.Path.Combine(Application.StartupPath, "test.xls");
+            OleDbConnection connection = new OleDbConnection(@"provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + SourseFilePath + "';Extended Properties=Excel 8.0;");
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при открытии прайс-листа!");
+                return;
+            }
+            String err = "";
+            List<Price> priceList = ReadFromExcel(connection, out err);
+
+           if (String.IsNullOrEmpty(err) == false)
+           {
+               MessageBox.Show("Ошибка при чтении прайс-листа! " + err);
+               return;
+           }
+            string path = Application.StartupPath + @"\temp";
+            System.IO.Directory.CreateDirectory(path);
+            DateTime todayDate = DateTime.Today;
+            string date_string = todayDate.Day.ToString() + todayDate.Month.ToString() + todayDate.Year.ToString();
+            string FilePath = path + @"\ЛНКосметика + " + date_string + ".txt";
+           
+
+            CreateTextFile(FilePath, priceList, out err);
+            if (String.IsNullOrEmpty(err) == false)
+            {
+                MessageBox.Show("Ошибка при создании файла! " + err);
+                return;
+            }
+            try
+            {
+                string pathZip = Application.StartupPath + @"\temp_zip";
+
+                if (Directory.Exists(pathZip) == false) System.IO.Directory.CreateDirectory(pathZip);
+
+                string zipPath = pathZip + @"\price_arch.zip";
+                if (File.Exists(zipPath)) File.Delete(zipPath);
+
+                ZipFile.CreateFromDirectory(path, zipPath);
+                if (Directory.Exists(path)) Directory.Delete(path, true);
+
+                string m = "Архив находится в папке " + zipPath;
+                label2.Visible = true;
+                label2.Text = m;
+
+                string ftpText = ftpNameTextBox.Text;
+                if (String.IsNullOrEmpty(ftpText))
+                {
+                    MessageBox.Show("Не указан адрес ftp-сервера");
+                    return;
+                }
+                else
+                {
+                    try
+                    {
+                        SendToFTP(zipPath, ftpText);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка соединения с ftp-сервером. Проверьте адрес и настройки соединения. Архив создан на диске");
+                        return;
+                    }
+
+                }
+
+                MessageBox.Show("Все!");
+            }
+            catch
+            {
+                MessageBox.Show("Произошла непредвиденная ошибка");
+                return;
+            }
+        }
+    }
+}
---
+++ Test_LN_Cosmetics/Price.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_LN_Cosmetics
+{
+   public class Price
+    {
+       public int Cod { get; set; }
+       public string Name { get; set; }
+       public string MNF { get; set; }
+       public string CNTR { get; set; }
+       public DateTime Srok { get; set; }
+       public int Kol { get; set; }
+       public decimal Cena { get; set; }
+       public int  Kratnost { get; set; }
+       public Int64 Barcode { get; set; }
+       public int Ratends { get; set; }
+
+       public Price()
+       {
+           this.Cod = 0;
+           this.Name = "";
+           this.MNF = "";
+           this.CNTR = "";
+           this.Srok = DateTime.Parse("01/01/0001");
+           this.Kol = 0;
+           this.Cena = 0;
+           this.Kratnost = 1;
+           this.Barcode = 0;
+           this.Ratends = 18;
+       }
+    }
+}
